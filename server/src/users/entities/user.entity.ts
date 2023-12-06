a07fc1825c1cb4862579f3d5f56c2da514219be5
Irina <belkasonjas@gmail.com>
2023-12-06 20:00:16 +0300

    linting

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,20 +1,19 @@
 import { BaseEntity } from '@src/db/base-entity/base.entity';
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
-import {Field, ID, ObjectType} from "@nestjs/graphql";
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @Entity()
 @ObjectType()
 export class User extends BaseEntity {
-    @Field(() => ID)
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Field()
-    @Column()
-    email: string
+  @Field()
+  @Column()
+  email: string;
 
-    @Field({ nullable: true })
-    @Column({ nullable: true })
-    name: string
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  name: string;
 }
--- server/src/users/resolvers/users.resolver.ts
+++ server/src/users/resolvers/users.resolver.ts
@@ -1,38 +1,41 @@
-import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
-import {User} from "../entities/user.entity";
-import {UsersService} from "../services/users.service";
-import {CreateUserInput} from "../inputs/user-create.input";
-import {UpdateUserInput} from "../inputs/user-update.input";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+import { UsersService } from '../services/users.service';
+import { CreateUserInput } from '../inputs/user-create.input';
+import { UpdateUserInput } from '../inputs/user-update.input';
 
 @Resolver('Users')
 export class UsersResolver {
-    constructor(
-        private readonly userService: UsersService,
-    ) {
-    }
+  constructor(private readonly userService: UsersService) {}
 
-    @Mutation(() => User)
-    async createUser(@Args({name: "dto", type: () => CreateUserInput}) createUserInput: CreateUserInput): Promise<User> {
-        return await this.userService.createUser(createUserInput)
-    }
+  @Mutation(() => User)
+  async createUser(
+    @Args({ name: 'dto', type: () => CreateUserInput })
+    createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return await this.userService.createUser(createUserInput);
+  }
 
-    @Mutation(() => User)
-    async updateUser(@Args({name: "dto", type: () => UpdateUserInput}) updateUserInput: UpdateUserInput): Promise<User> {
-        return await this.userService.updateUser(updateUserInput)
-    }
+  @Mutation(() => User)
+  async updateUser(
+    @Args({ name: 'dto', type: () => UpdateUserInput })
+    updateUserInput: UpdateUserInput,
+  ): Promise<User> {
+    return await this.userService.updateUser(updateUserInput);
+  }
 
-    @Mutation(() => Number)
-    async removeUser(@Args('id') id: string): Promise<string> {
-        return await this.userService.removeUser(id)
-    }
+  @Mutation(() => Number)
+  async removeUser(@Args('id') id: string): Promise<string> {
+    return await this.userService.removeUser(id);
+  }
 
-    @Query(() => User)
-    async getOneUser(@Args('id') id: string): Promise<User> {
-        return await this.userService.getOneUser(id)
-    }
+  @Query(() => User)
+  async getOneUser(@Args('id') id: string): Promise<User> {
+    return await this.userService.getOneUser(id);
+  }
 
-    @Query(() => [ User ])
-    async getAllUsers(): Promise<User[]> {
-        return await this.userService.getAllUsers()
-    }
+  @Query(() => [User])
+  async getAllUsers(): Promise<User[]> {
+    return await this.userService.getAllUsers();
+  }
 }
