ac573a0efad88841f667d1da21a58095253bd3d1
Irina <irina.puliak@gmail.com>
2024-03-14 12:58:47 +0300

    update deploy.yaml

---
+++ infra/env/dev/deploy/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    #    encrypt        = true
+    bucket = "terraform-states-2024" // Bucket where to SAVE Terraform State
+    key    = "dev/terraform_deploy.tfstate" // Object name in the bucket to SAVE Terraform State
+    region = "eu-central-1"          // Region where bucket created
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
